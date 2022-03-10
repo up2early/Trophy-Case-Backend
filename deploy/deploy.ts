7385770f32ae67a7c75f0337933c2e22286909cf
up2early <wakeup2early@gmail.com>
2022-03-09 20:59:31 -0700

    Added env file for deploy private key

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -2,13 +2,14 @@ import { utils, Wallet } from "zksync-web3";
 import * as ethers from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+import "secrets";
 
 // An example of a deploy script that will deploy and call a simple contract.
 export default async function (hre: HardhatRuntimeEnvironment) {
   console.log(`Running deploy script for the Greeter contract`);
 
   // Initialize the wallet.
-  const wallet = new Wallet("<WALLET-PRIVATE-KEY>");
+  const wallet = new Wallet(process.env.PRIVATE_KEY);
 
   // Create deployer object and load the artifact of the contract we want to deploy.
   const deployer = new Deployer(hre, wallet);
